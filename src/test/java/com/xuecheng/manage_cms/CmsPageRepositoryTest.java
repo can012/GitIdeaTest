079f028a7993cd2b40395371c34cbe94ff8e7c9e
liucancan <cancanliu97@gmail.com>
2019-02-17 00:15:17 +0800

    2019年2月17日00:14:47  第三天完成

--- src/test/java/com/xuecheng/manage_cms/CmsPageRepositoryTest.java
+++ src/test/java/com/xuecheng/manage_cms/CmsPageRepositoryTest.java
@@ -7,9 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -28,18 +26,17 @@ public class CmsPageRepositoryTest {
     CmsPageRepository cmsPageRepository;
 
     @Test
-    public void testFindPage(){
+    public void testFindPage() {
         int page = 0;//从0开始
         int size = 10;//每页记录数
-        Pageable pageable = PageRequest.of(page,size);
+        Pageable pageable = PageRequest.of(page, size);
         Page<CmsPage> all = cmsPageRepository.findAll(pageable);
         System.out.println(all);
     }
 
-
     //添加
     @Test
-    public void testInsert(){
+    public void testInsert() {
         //定义实体类
         CmsPage cmsPage = new CmsPage();
         cmsPage.setSiteId("s01");
@@ -62,10 +59,33 @@ public class CmsPageRepositoryTest {
     @Test
     public void testUpdate() {
         Optional<CmsPage> optional = cmsPageRepository.findById("5c31cae2e1d04b531c836e9d");
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             CmsPage cmsPage = optional.get();
             cmsPage.setPageName("测试页面01");
             cmsPageRepository.save(cmsPage);
         }
+    }
+
+    //自定义条件查询测试
+    @Test
+    public void testFindAll() {
+        //条件匹配器
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching();
+        exampleMatcher = exampleMatcher.withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+        //页面别名模糊查询，需要自定义字符串的匹配器实现模糊查询
+        //ExampleMatcher.GenericPropertyMatchers.contains() 包含
+        //ExampleMatcher.GenericPropertyMatchers.startsWith()//开头匹配
+        //条件值
+        CmsPage cmsPage = new CmsPage();
+        //站点ID
+        cmsPage.setSiteId("5a751fab6abb5044e0d19ea1");
+        //模板ID
+        cmsPage.setTemplateId("5a962c16b00ffc514038fafd");
+//        cmsPage.setPageAliase("分类导航");
+        //创建条件实例
+        Example<CmsPage> example = Example.of(cmsPage, exampleMatcher);
+        Pageable pageable = new PageRequest(0, 10);
+        Page<CmsPage> all = cmsPageRepository.findAll(example, pageable);
+        System.out.println(all);
     }
 }
